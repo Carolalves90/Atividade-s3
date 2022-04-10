b3146bf6acae2f56a41ed10b44d0e64213a4abd4
carolalves <prof.carolalves90@gmail.com>
2022-04-10 16:19:33 -0300

    resolução dia da semana

--- diadasemana.js
+++ diadasemana.js
@@ -1,2 +1,32 @@
 //Elabore um algoritmo que receba um número (1-7) 
 //e devolva o dia da semana correspondente.
+
+function verificaDia (diaDaSemana){
+    switch (diaDaSemana){
+        case '1':
+            return 'Domingo'
+            break
+        case '2':
+            return 'Segunda'
+            break
+        case '3':
+            return 'Terça'
+            break
+        case '4':
+            return 'Quarta'
+            break
+        case '5':
+            return 'Quinta'
+            break
+        case '6':
+            return 'Sexta'
+            break
+        case '7':
+            return 'Sábado'
+            break
+        default:
+            return 'Não é dia da Semana' 
+    }
+}
+
+console.log(verificaDia('8'))

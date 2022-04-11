21a134ade8315c6540044981236b7c3fd80f2452
carolalves <prof.carolalves90@gmail.com>
2022-04-10 21:43:22 -0300

    resolução numero

--- numero.js
+++ numero.js
@@ -1,2 +1,14 @@
 //Elabore um algoritmo que receba dois números e determine qual é o maior entre eles,
 // se os números forem iguais, mostre uma mensagem no console "Os números são iguais".
+
+function maiorDeDois(num1, num2){
+    if(num1 > num2){
+        return `${num1} é maior`
+    } else if(num1 < num2){
+        return `${num2} é maior`
+    } else if(num1 == num2){
+        return 'Os números são iguais'
+    }
+}
+
+console.log(maiorDeDois(3,3))

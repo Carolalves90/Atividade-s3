e408e7f0c182d561990dafa5c1007638af933b37
carolalves <prof.carolalves90@gmail.com>
2022-04-10 22:41:31 -0300

    maior de idade resolvido

--- maioridade.js
+++ maioridade.js
@@ -1,1 +1,11 @@
 //Crie uma função que recebe o ano de nascimento da pessoa informando se ela é maior de idade ou menor.
+
+function maiorDeIdade(anoNascimento){
+    if(anoNascimento < 2004){
+        return 'Maior de Idade'
+    } else if(anoNascimento >= 2004){
+        return 'menor de idade'
+    }
+}
+
+console.log(maiorDeIdade('2021'))

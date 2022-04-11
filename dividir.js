fd2dd50e4a59d54adcd13ec724f5c3fd72ca56af
carolalves <prof.carolalves90@gmail.com>
2022-04-11 12:01:03 -0300

    dividir resolvido

--- dividir.js
+++ dividir.js
@@ -1,2 +1,13 @@
 //Crie uma função que recebe 2 parâmetros e retorna o resultado da divisão entre eles. 
 //Além disso, se o resto dessa divisão for zero deverá imprimir no console o valor e dizer que ele é par.
+
+
+function dividirDois(n1, n2){
+    const resultado = n1/ n2
+     
+       if(n1 % n2 == 0){
+        return `${resultado} é Par`
+    }
+}
+
+console.log(dividirDois(20,5))

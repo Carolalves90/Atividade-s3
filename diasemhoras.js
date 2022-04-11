90660594c24a15569d025e5aad9954cb145c6ceb
carolalves <prof.carolalves90@gmail.com>
2022-04-10 22:51:58 -0300

    Conversão dias em Horas resolvido

--- diasemhoras.js
+++ diasemhoras.js
@@ -1,1 +1,9 @@
 //crie um algoritmo que converte dias em horas, quando recebe um número de dias.
+
+function diasEmHoras (numeroDia){
+    const diaHora = numeroDia * 24
+
+        return `São ${diaHora} horas`
+}
+
+console.log(diasEmHoras(4))

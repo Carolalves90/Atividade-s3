3c3cba98bdc37c6a451b3141bcbd2d6c0d03dcd4
carolalves <prof.carolalves90@gmail.com>
2022-04-11 11:14:49 -0300

    Par Impar resolvido

--- parimpar.js
+++ parimpar.js
@@ -1,1 +1,11 @@
 //Crie uma função que retorna a palavra (impar/par) de acordo com seu parâmetro.
+
+function parImpar(numero){
+    if(numero % 2 === 0){
+        return 'Par'
+    } else if(numero % 2 != 0){
+        return 'Impar'
+    }
+}
+
+console.log(parImpar('37'))
